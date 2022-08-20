7e6233510dadfeb3eb957ac3eb458c81f9854893
zakaria <erfan.yekezare@gmail.com>
2022-08-20 12:47:26 +0430

    custom user model and its custom forms has been created

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from pyexpat import model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ("age",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
