a59e383b8a58d7e497e9b72024bc91cab269b851
zakaria <erfan.yekezare@gmail.com>
2022-08-28 10:28:52 +0430

    create comment model and migrate

--- articles/models.py
+++ articles/models.py
@@ -14,3 +14,15 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    context = models.TextField(max_length=300)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.context
+
+    def get_absolute_url(self):
+        return reverse("article_list")
