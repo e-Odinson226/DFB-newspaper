ba0b6a603f332d8359443388f57dd2fd65e6460d
zakaria <erfan.yekezare@gmail.com>
2022-08-28 14:16:14 +0430

    define comment post class in [views.py] add it to [ArticleDetailView] which is a wrapper

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
     CreateView,
     DeleteView,
     DetailView,
     UpdateView,
+    FormView,
 )
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+
 from .models import Article
 from .forms import CommentForm
 
@@ -16,7 +20,7 @@ class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "articles_list.html"
 
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class CommentGet(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
@@ -24,6 +28,36 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
         context = super().get_context_data()
         context["form"] = CommentForm()
         return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    def get(sel, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(sel, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class ArticleDeleteView(LoginRequiredMixin, DeleteView):
